22d8e04b7a586ed909dd1d7d8dc1a336fe3c4f2b
Michael Bateman <michael.bateman.com@gmail.com>
2015-04-21 14:31:55 +0100

    first failing test

---
+++ src/main/java/com/metability/stationsearch/StationFinder.java
@@ -0,0 +1,12 @@
+package com.metability.stationsearch;
+
+import java.util.List;
+
+/**
+ * Created by michael on 21/04/2015.
+ */
+public class StationFinder {
+    public static List<String> lookup(String phrase) {
+        return null;
+    }
+}
