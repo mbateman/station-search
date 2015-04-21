b2d42bd8864e8d8cff9700c9fb163d8e34fe53f0
Michael Bateman <michael.bateman.com@gmail.com>
2015-04-21 15:40:46 +0100

    first passing test

--- src/main/java/com/metability/stationsearch/StationFinder.java
+++ src/main/java/com/metability/stationsearch/StationFinder.java
@@ -1,12 +1,18 @@
 package com.metability.stationsearch;
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 /**
  * Created by michael on 21/04/2015.
  */
 public class StationFinder {
+    static List<String> stations = Arrays.asList(new String[]{"DARTFORD", "DARTMOUTH" });
+
     public static List<String> lookup(String phrase) {
-        return null;
+       List<String> nextCharacters = stations.stream().map(station -> station.substring(phrase.length(), phrase.length() + 1)).collect(Collectors.toList());
+       return Stream.concat(nextCharacters.stream(), stations.stream().filter(station -> station.contains(phrase))).collect(Collectors.toList());
     }
 }
